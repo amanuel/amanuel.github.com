a30020b7c4b0d3713091ba14ee6726d8f87c6a20
Amanuel Tewolde <github@ticklespace.com>
2012-02-12 16:33:25 -0500

    fix script

--- forim.js
+++ forim.js
@@ -1,9 +1,8 @@
-<script type="text/javascript">
 if (typeof jQuery != 'undefined') {
     jQuery(document).ready(function($) {
         var filetypes = /\.(zip|exe|pdf|doc*|xls*|ppt*|mp3|safariextz)$/i;
         var baseHref = '';
-        if (jQuery('base').attr('href') != undefined)
+        if (jQuery('base').attr('href') !== undefined)
             baseHref = jQuery('base').attr('href');
         jQuery('a').each(function() {
             var href = jQuery(this).attr('href');
@@ -11,7 +10,7 @@ if (typeof jQuery != 'undefined') {
                 jQuery(this).click(function() {
                     var extLink = href.replace(/^https?\:\/\//i, '');
                     _gaq.push(['_trackEvent', 'External', 'Click', extLink]);
-                    if (jQuery(this).attr('target') != undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
+                    if (jQuery(this).attr('target') !== undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
                         setTimeout(function() { location.href = href; }, 200);
                         return false;
                     }
@@ -28,7 +27,7 @@ if (typeof jQuery != 'undefined') {
                     var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
                     var filePath = href;
                     _gaq.push(['_trackEvent', 'Download', 'Click-' + extension, filePath]);
-                    if (jQuery(this).attr('target') != undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
+                    if (jQuery(this).attr('target') !== undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
                         setTimeout(function() { location.href = baseHref + href; }, 200);
                         return false;
                     }
@@ -37,4 +36,3 @@ if (typeof jQuery != 'undefined') {
         });
     });
 }
-</script>
