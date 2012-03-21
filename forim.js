dff07977c6ca9b2268958c840c932a93b3813ba4
amanuel <amanuel@ticklespace.com>
2012-03-21 11:53:56 -0400

    improve track_links

--- forim.js
+++ forim.js
@@ -1,1 +1,1 @@
-mixpanel.track_links("a", "Clicked link", function(ele) { return { type: $(ele).attr('type')};});
+mixpanel.track_links("a", "Clicked link", function(ele) { return { 'type': $(ele).attr('href')};});
